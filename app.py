2b3af67ea53fb4bcf8e5c29d25b56169e975d00d
Irfan Saf <irfansaf7@gmail.com>
2024-04-24 21:53:57 +0700

    adding scripts to npm

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
 
+from inference.load_data import load_data
+
 app = Flask(__name__)
 
+# Load data
+data = load_data("harga_cleaned.csv")
+print(data.head())
 
 @app.route('/')
-def hello_world():  # put application's code here
+def home():
     return render_template('index.html')
 
 
